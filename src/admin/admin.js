c88d9fe226516bbb97b8d96691199955a68e4445
Gustavo Pateti <gpateti@gmail.com>
2020-04-14 23:31:21 -0400

    Fix key cert

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -3,18 +3,9 @@ import * as admin from "firebase-admin";
 const isProduction = process.env.NODE_ENV === "production";
 const prefix = isProduction ? "PROD" : "DEV";
 
-const serviceAccount = {
-  type: process.env[`${prefix}_ADMIN_TYPE`],
-  project_id: process.env[`${prefix}_ADMIN_PROJECT_ID`],
-  private_key_id: process.env[`${prefix}_ADMIN_PRIVATE_KEY_ID`],
-  private_key: process.env[`${prefix}_ADMIN_PRIVATE_KEY`],
-  client_email: process.env[`${prefix}_ADMIN_CLIENT_EMAIL`],
-  client_id: process.env[`${prefix}_ADMIN_CLIENT_ID`],
-  auth_uri: process.env[`${prefix}_ADMIN_AUTH_URI`],
-  token_uri: process.env[`${prefix}_ADMIN_TOKEN_URI`],
-  auth_provider_x509_cert_url: process.env[`${prefix}_ADMIN_AUTH_PROVIDER_URL`],
-  client_x509_cert_url: process.env[`${prefix}_ADMIN_CLIENT_URL`],
-};
+const serviceAccount = isProduction
+  ? require("../../admin-key.json")
+  : require("../../admin-key-dev.json");
 
 const databaseURL = process.env[`${prefix}_DATABASE_URL`];
 
